6ae947f171e1c88e5b4cc8675368f2479c2e5eae
Felipe <menfelipe1@gmail.com>
2024-09-04 17:28:01 -0300

    Adicionando novas abas no nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -7,11 +7,15 @@ const Nav = () => {
         <>
             <NavStyle>
                 <header>
-                <div>
-                    <Link to={'/'}>Home</Link>
-                    <Link to={'/login'}>Login</Link>
-                    <Link to={'/produtos'}>Produtos</Link>
-                </div>
+                    <div className="logo">
+                    </div>
+                    <div className="links">
+                        <Link to={'/'} className="caminhos">Home</Link>
+                        <Link to={'/produtos'} className="caminhos">Produtos</Link>
+                        <Link to={'/sobre'} className="caminhos">Sobre</Link>
+                        <Link to={'/contato'} className="caminhos">Contato</Link>
+                        <Link to={'/login'} className="caminhos">Login</Link>
+                    </div>
                 </header>
             </NavStyle>
         </>
